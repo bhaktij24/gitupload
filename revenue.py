f1953d10e91614b168618b4440e990c46187d33e
UmaAnantharaman <44247436+UmaAnantharaman@users.noreply.github.com>
2018-11-19 21:07:56 -0500

    Update revenue.py

--- revenue.py
+++ revenue.py
@@ -24,13 +24,18 @@ class revenue:
         self.movie_release_year = []
         self.count_noofmovies_each_genre =[]
         self.movie_title_year=[]
-              
+        self.sum_revenues =[]
+        self.movies_genre =[]
+        self.labells = []
+        self.sizes = []
+        self.row =[]
+        self.p1=[]
+        self.p2=[]
+                      
     def movies_particular_year(self):
        
         #GRAPH 1 - ALL MOVIES IN A PARTICULAR YEAR
         curObj_movie_year = conn.execute("SELECT strftime('%Y',movie_releaseDate),count(movie_title) as Year from movies_metadata group by strftime('%Y',movie_releaseDate)")
-        #release_year = []
-        #noof_movies_released = []
         for row in curObj_movie_year:
             self.release_year = self.release_year + [row[0]]
             self.noof_movies_released = self.noof_movies_released + [row[1]]
@@ -47,11 +52,10 @@ class revenue:
 
     def revenues_particular_genre(self):
         #GRAPH 2 - ALL REVENUE FOR A PARTICULAR GENRE
-        curObj_revenue_sum = conn.execute("SELECT MM.movie_genre,strftime('%Y',MM.movie_releaseDate) as Year,sum(MR.revenue) as Total_Revenue from movies_metadata MM inner join revenue MR on MM.movie_title=MR.movie_title GROUP BY MM.movie_genre,strftime('%Y',MM.movie_releaseDate) order by MM.movie_genre,strftime('%Y',MM.movie_releaseDate)")
-       # sum_revenues_each_genre =[]
-       # movies_each_genre = []
-       # movie_title_year=[]
-
+        curObj_revenue_sum = conn.execute("SELECT MM.movie_genre,strftime('%Y',MM.movie_releaseDate) as Year,sum(MR.revenue) as Total_Revenue from\
+                                        movies_metadata MM inner join revenue MR on MM.movie_title=MR.movie_title GROUP BY MM.movie_genre,\
+                                        strftime('%Y',MM.movie_releaseDate) order by MM.movie_genre,strftime('%Y',MM.movie_releaseDate)")
+    
         for row in curObj_revenue_sum:
             self.movies_each_genre = self.movies_each_genre + [row[0]]
             self.movie_title_year = self.movie_title_year + [row[1]]
@@ -63,53 +67,55 @@ class revenue:
         self.movies_genre = list(self.movies_each_genre)
         self.sum_revenues = list(self.sum_revenues_each_genre)
 
+        df = pd.DataFrame({'MOVIE GENRE' : self.movies_each_genre,
+                            'YEAR RELEASED':self.movie_title_year,
+                            'REVENUE YIELDED(IN MILLION DOLLARS)':self.sum_revenues_each_genre})
 
+        pd.options.display.max_columns = None
+        print(tabulate(df, headers='keys', tablefmt="orgtbl"))
 
-        """womenMeans = (movies_genre)
-        menMeans = (sum_revenues)
-        indices = [movie_title_year]
-        #Calculate optimal width
-        width = min(diff(indices))/3
-
-        fig = plt.figure()
-        ax = fig.add_subplot(111)
-        ax.bar(indices-width,womenMeans,width,color='b',label='-Ymin')
-        ax.bar(indices,menMeans,width,color='r',label='Ymax')
-        ax.set_xlabel('Bar graph Comparison - Revenue and Movie Genre in a particular year')
-        plt.show()"""
+        #plt.figure();
+        #df.plot.hist(stacked=True, bins=20)
+        #plt.show()
+        
         
     def genre_ofmovies_particularyear_comparison(self):
         #GRAPH 3 - GENRE OF MOVIES FOR A PARTICULAR YEAR
-        curObj_genre_count = conn.execute("SELECT movie_genre,strftime('%Y',movie_releaseDate) as Year,count(movies_id) from movies_metadata GROUP BY movie_genre,strftime('%Y',movie_releaseDate);")
-        self.movie_release_year = []
-        self.count_noofmovies_each_genre =[]
-        self.movies_each_genre = []
-        self.labels = []
-        self.sizes = []
-        self.row =[]
+        font = {'family': 'normal','weight': 'bold','size': 5}
+        curObj_genre_count_year1 = conn.execute("SELECT MM.movie_genre,count(MM.movies_id) from \
+                                           movies_metadata MM where  \
+                                           strftime('%Y',MM.movie_releaseDate) = '2017'\
+                                           GROUP BY MM.movie_genre,strftime('%Y',MM.movie_releaseDate) order by MM.movie_genre,strftime('%Y',MM.movie_releaseDate);")
+        resultRatings1 = curObj_genre_count_year1.fetchall()
+        print(resultRatings1)
+        
+        curObj_genre_count_year2 = conn.execute("SELECT MM.movie_genre,count(MM.movies_id) from \
+                                           movies_metadata MM where  \
+                                           strftime('%Y',MM.movie_releaseDate) = '2018'\
+                                           GROUP BY MM.movie_genre,strftime('%Y',MM.movie_releaseDate) order by MM.movie_genre,strftime('%Y',MM.movie_releaseDate);")
+        resultRatings2 = curObj_genre_count_year2.fetchall()
+        print(resultRatings2)
+        
+        genre17, count17 = zip(*resultRatings1)
+        genre18,count18 = zip(*resultRatings2)
 
-        for row in curObj_genre_count:
-            print("movie_genre",row[0])
-            self.movies_each_genre = self.movies_each_genre + [row[0]]
-           
-            self.movie_release_year = self.movie_release_year  + [row[1]]
-            print("movie_release_year",row[1])
-            
-            self.count_noofmovies_each_genre = self.count_noofmovies_each_genre + [row[2]]
-            print("count",row[2])
-            
-            self.labels = list(self.movies_each_genre)
-            self.sizes = list(self.count_noofmovies_each_genre)
-            self.row = list(self.movie_release_year)
-            
+        # Make figure and axes
+        fig, axs = plt.subplots(2,2)
+
+
         fig = plt.figure()
         ax1 = fig.add_axes([0, 0, .5, .5], aspect=1)
-        ax1.pie(sizes,explode=None, labels=labels, autopct='%1.1f%%',shadow=True, startangle=90, radius = 1.2)
-        plt.title(row, bbox={'facecolor':'0.8', 'pad':5})
+        ax1.pie(count17,explode=None,labels=genre17, autopct='%1.1f%%',shadow=True, startangle=90, radius = 1.2)
+        plt.title(2017, bbox={'facecolor':'0.8', 'pad':5})
         ax1.axis('equal')
 
+        ax2 = fig.add_axes([.5, .0, .5, .5], aspect=1)
+        ax2.pie(count18,explode=None,labels=genre18, autopct='%1.1f%%',shadow=True, startangle=90, radius = 1.2)
+        plt.title(2018, bbox={'facecolor':'0.8', 'pad':5})
+        ax2.axis('equal')
         plt.show()
 
+       
     def revenues_particular_year(self):
 #GRAPH 4 - REVENUES FOR A PARTICULAR YEAR
         curObj_revenue_year = conn.execute("SELECT MM.movie_title,strftime('%Y',MM.movie_releaseDate) as Year,sum(MR.revenue) as Total_Revenue from movies_metadata MM inner join revenue MR on MM.movie_title=MR.movie_title GROUP BY MM.movie_genre,strftime('%Y',MM.movie_releaseDate)")
@@ -133,14 +139,14 @@ class revenue:
                                'YEAR RELEASED':self.movie_release_year})
 
             pd.options.display.max_columns = None
-            print(tabulate(df, headers='keys', tablefmt="orgtbl"))
+        print(tabulate(df, headers='keys', tablefmt="orgtbl"))
 
             
-"""r = revenue()
+r = revenue()
 r.movies_particular_year()
 r.revenues_particular_genre()
 r.genre_ofmovies_particularyear_comparison()
-r.revenues_particular_year()"""
+r.revenues_particular_year()
 
         
 
