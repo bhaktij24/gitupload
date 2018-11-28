aa5cbd9d447914d6ae43cd0c086614872efcf8b0
UmaAnantharaman <44247436+UmaAnantharaman@users.noreply.github.com>
2018-11-28 16:27:53 -0500

    Update AdminFunction.py

--- AdminFunction.py
+++ AdminFunction.py
@@ -33,47 +33,46 @@ class AdminFunction:
                                                4.Analysis of Revenue Yielded(In million dollars) for a particular year\n")
                             
 
-                while True:
+               while True:
                     #Creating an object 'my' and accessing the class revenue from revenue.py file
                     my = revenue.revenue()
-                    #Admin enters option 1 and chooses to view All movies released in a year
                     if revenue_graph_to_view  == "1":
                         my.movies_particular_year()
                         break
-                    
+
                     #Admin enters option 2 and chooses to view Revenue Yielded(In million dollars) for a particular genre
                     elif revenue_graph_to_view  == "2":
                         my.revenues_particular_genre()
                         break
-                    
+
                     #Admin enters option 3 and chooses to view Genre of movies for a particular year
                     elif revenue_graph_to_view  == "3":
                         my.genre_ofmovies_particularyear_comparison()
                         break
-                    
+
                     #Admin enters option 4 and chooses to view Revenue Yielded(In million dollars) for a particular year
                     elif revenue_graph_to_view  == "4":
                         my.revenues_particular_year()
                         break
-                    
+
                     #Admin enters invalid options
                     else:
                         print("No such action available! Please try to login again for continuation")
                         break
                         return my
 
-            #Admin enters Comparison option and is asked to choose one from the 5 different analysis graphs
             elif Admin_Task.lower() == "comparison" or Admin_Task.lower() == "c":
+
                 #Takes input from Admin
-                comparison_graph_to_view = input("What kind of graph do you want to view?[y/n]\n\
-                                                  Enter your Choice:\n\
-                                                  1.Comparison Analysis of Budget allocated and Profit earned for all movies\n\
-                                                  2.Comparison Analysis of Genre of movies that had maximum good reviews and performed well\n\
-                                                  3.Comparison Analysis of Genre of movies that had maximum poor reviews and faced heavy criticism by End Users\n\
-                                                  4.Comparison Analysis of Least and most Preferred movies for each year\n\
-                                                  5.Comparison Analysis of Number of successful days movies ran in theatres\n")
+                comparison_graph_to_view = input("Enter your Choice:\n \
+                                               1.Comparison of Budget and Profit for all movies\n\
+                                               2.Comparison of Genre and positive reviews\n \
+                                               3.Comparison of Genre and negative reviews\n\
+                                               4.Comparison of least and most preferred movies for each year\n\
+                                               5.Comparison of Number of successful days movies ran in theatres\n")
 
                 while True:
+                    
                     #Creating an object 'comp' and accessing the class comparison from Comparison.py file
                     comp = Comparison.comparison()
 
@@ -104,21 +103,18 @@ class AdminFunction:
 
                     #Admin enters invalid options
                     else:
-                        print("No such action available! Please try to login again for continuation")
+                        print("No such action available! Please try to login again for continuation!")
                         break
                         return comp
 
             else:
-                print("Admin Function Over,Good bye")
-                break
-
-
-
-
-
+                answer = input("Invalid input! Do you still want to continue[y]?: ")
+                if answer.lower() == "y":
+                    continue
+                else:
+                    print("You chose to logout of the system.")
+                    break
                 
-                
-                
-                
-  
-        
+if __name__ == "__main__":
+    print("Sorry, to access admin module. Please login with administrator credentials to the system.")
+    exit()
